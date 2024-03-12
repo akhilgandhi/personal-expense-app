a962502232fce8ce5e963a8456dd12e8a9d535e1
akhilgandhi <akhilgandhi@live.in>
2024-03-12 22:39:27 +0530

    Added automated and semi-automated unit tests

--- microservices/dashboard-service/src/test/java/com/akhil/microservices/composite/dashboard/DashboardServiceApplicationTests.java
+++ microservices/dashboard-service/src/test/java/com/akhil/microservices/composite/dashboard/DashboardServiceApplicationTests.java
@@ -1,13 +1,107 @@
 package com.akhil.microservices.composite.dashboard;
 
+import com.akhil.microservices.api.core.account.Account;
+import com.akhil.microservices.api.core.expense.Category;
+import com.akhil.microservices.api.core.expense.Expense;
+import com.akhil.microservices.api.core.expense.PaymentMode;
+import com.akhil.microservices.api.exceptions.InvalidInputException;
+import com.akhil.microservices.api.exceptions.NotFoundException;
+import com.akhil.microservices.composite.dashboard.services.DashboardCompositeIntegration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DashboardServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static final int ACCOUNT_ID_OK = 1;
+	private static final int ACCOUNT_ID_NOT_FOUND = 2;
+	private static final int ACCOUNT_ID_INVALID = 3;
+
+	@Autowired
+	private WebTestClient client;
+
+	@MockBean
+	private DashboardCompositeIntegration integration;
+
+	@BeforeEach
+	void setUp() {
+
+		when(integration.getAccount(ACCOUNT_ID_OK))
+				.thenReturn(new Account(ACCOUNT_ID_OK, "name", "mock-address"));
+		when(integration.getExpenses(ACCOUNT_ID_OK))
+				.thenReturn(Collections.singletonList(
+						new Expense(
+								ACCOUNT_ID_OK,
+								1,
+								LocalDateTime.now(),
+								10.0,
+								new Category("Category 1", true),
+								"Account 1",
+								PaymentMode.CASH,
+								Optional.empty(),
+								"mock-address")));
+
+		when(integration.getAccount(ACCOUNT_ID_NOT_FOUND))
+				.thenThrow(new NotFoundException("NOT FOUND: " + ACCOUNT_ID_NOT_FOUND));
+
+		when(integration.getAccount(ACCOUNT_ID_INVALID))
+				.thenThrow(new InvalidInputException("INVALID: " + ACCOUNT_ID_INVALID));
 	}
 
+	@Test
+	void contextLoads() {}
+
+	@Test
+	void getAccountById() {
+
+		client.get()
+				.uri("/dashboard/" + ACCOUNT_ID_OK)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.accountId").isEqualTo(ACCOUNT_ID_OK)
+				.jsonPath("$.expenses.length()").isEqualTo(1);
+	}
+
+	@Test
+	void getAccountNotFound() {
+
+		client.get()
+				.uri("/dashboard/" + ACCOUNT_ID_NOT_FOUND)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/dashboard/" + ACCOUNT_ID_NOT_FOUND)
+				.jsonPath("$.message").isEqualTo("NOT FOUND: " + ACCOUNT_ID_NOT_FOUND);
+	}
+
+	@Test
+	void getAccountInvalidInput() {
+
+		client.get()
+				.uri("/dashboard/" + ACCOUNT_ID_INVALID)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/dashboard/" + ACCOUNT_ID_INVALID)
+				.jsonPath("$.message").isEqualTo("INVALID: " + ACCOUNT_ID_INVALID);
+	}
 }
---
+++ test-all.bash
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+#
+# Sample usage:
+#
+#   HOST=localhost PORT=8081 ./test-em-all.bash
+#
+: ${HOST=localhost}
+: ${PORT=8081}
+: ${ACCOUNT_ID_EXPS=1}
+: ${ACCOUNT_ID_NOT_FOUND=13}
+: ${ACCOUNT_NO_EXPS=1}
+
+function assertCurl() {
+
+  local expectedHttpCode=$1
+  local curlCmd="$2 -w \"%{http_code}\""
+  local result=$(eval $curlCmd)
+  local httpCode="${result:(-3)}"
+  RESPONSE='' && (( ${#result} > 3 )) && RESPONSE="${result%???}"
+
+  if [ "$httpCode" = "$expectedHttpCode" ]
+  then
+    if [ "$httpCode" = "200" ]
+    then
+      echo "Test OK (HTTP Code: $httpCode)"
+    else
+      echo "Test OK (HTTP Code: $httpCode, $RESPONSE)"
+    fi
+  else
+    echo  "Test FAILED, EXPECTED HTTP Code: $expectedHttpCode, GOT: $httpCode, WILL ABORT!"
+    echo  "- Failing command: $curlCmd"
+    echo  "- Response Body: $RESPONSE"
+    exit 1
+  fi
+}
+
+function assertEqual() {
+
+  local expected=$1
+  local actual=$2
+
+  if [ "$actual" = "$expected" ]
+  then
+    echo "Test OK (actual value: $actual)"
+  else
+    echo "Test FAILED, EXPECTED VALUE: $expected, ACTUAL VALUE: $actual, WILL ABORT"
+    exit 1
+  fi
+}
+
+set -e
+
+echo "HOST=${HOST}"
+echo "PORT=${PORT}"
+
+
+# Verify that a normal request works, expect two expenses
+assertCurl 200 "curl http://$HOST:$PORT/dashboard/$ACCOUNT_ID_EXPS -s"
+assertEqual $ACCOUNT_ID_EXPS $(echo $RESPONSE | jq .accountId)
+assertEqual 2 $(echo $RESPONSE | jq ".expenses | length")
+
+# Verify that a 404 (Not Found) error is returned for a non-existing accountId ($ACCOUNT_ID_NOT_FOUND)
+assertCurl 404 "curl http://$HOST:$PORT/dashboard/$ACCOUNT_ID_NOT_FOUND -s"
+assertEqual "No account found for accountId: $ACCOUNT_ID_NOT_FOUND" "$(echo $RESPONSE | jq -r .message)"
+
+# Verify that no expenses are returned for accountId $ACCOUNT_NO_EXPS
+assertCurl 200 "curl http://$HOST:$PORT/dashboard/$ACCOUNT_NO_EXPS -s"
+assertEqual $ACCOUNT_NO_EXPS $(echo $RESPONSE | jq .accountId)
+assertEqual 2 $(echo $RESPONSE | jq ".expenses | length")
+
+# Verify that a 422 (Unprocessable Entity) error is returned for a accountId that is out of range (-1)
+assertCurl 422 "curl http://$HOST:$PORT/dashboard/-1 -s"
+assertEqual "\"Invalid accountId: -1\"" "$(echo $RESPONSE | jq .message)"
+
+# Verify that a 400 (Bad Request) error error is returned for a accountId that is not a number, i.e. invalid format
+assertCurl 400 "curl http://$HOST:$PORT/dashboard/invalidProductId -s"
+assertEqual "null" "$(echo $RESPONSE | jq .message)"
+
+echo "End, all tests OK:" `date`
